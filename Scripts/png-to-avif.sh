35448953cab7690b800fdfdf835224356c445eff
Ryoko <Moenmyna@pm.me>
2024-08-06 16:57:43 -0400

    Scripts: Added comments to the scripts; Added functionality to move the leftover images to a new folder for user to review after conversion.

--- Scripts/png-to-avif.sh
+++ Scripts/png-to-avif.sh
@@ -1,3 +1,6 @@
 #! /bin/bash
 
+# Find all the .png files in the current working directory, invoke the magick command on each file, convert those files to .avif with the quality setting at "98", do this until all files have been converted.
 for f in ./*.png ; do magick "$f" -quality 98 "${f%.png}.avif" ; done
+# Move all the leftover .png files to a review folder after conversion is complete.
+mkdir _review && mv ./*.png _review
--- Scripts/webp-to-avif.sh
+++ Scripts/webp-to-avif.sh
@@ -1,3 +1,6 @@
 #! /bin/bash
 
+# Find all the .webp files in the current working directory, invoke the magick command on each file, convert those files to .avif with the quality setting at "98", do this until all files have been converted.
 for f in ./*.webp ; do magick "$f" -quality 98 "${f%.webp}.avif" ; done
+# Move all the leftover .webp files to a review folder after conversion is complete.
+mkdir _review && mv ./*.webp _review
