d846418089fa9ceacb6f76d8a4fda822fb99a239
Ryoko <Moenmyna@pm.me>
2024-08-22 18:51:35 -0400

    chore(Scripts): rename scripts so that they're easier to invoke in terminal
    * feat(jpg-jxl.sh): add comments explaining functionality as per other scripts

--- Scripts/AVIF/jpg-avif-r.sh
+++ Scripts/AVIF/jpg-avif-r.sh
@@ -3,6 +3,6 @@
 # Find all the .jpg files in the current working directory, invoke the magick command on each file, convert those files to .avif with the quality setting at "96", do this until all files have been converted.
 # "-resize" flag has been added for additional filesize reduction.
 # Modify "-resize" value between 80%-50% depending on amount of filesize you're trying to save.
-for f in ./*.jpg ; do magick "$f" -resize 75% -quality 96 "${f%.jpg}.avif" ; done
+for f in ./*.jp*g ; do magick "$f" -resize 75% -quality 96 "${f%.jp*g}.avif" ; done
 # Move all the leftover .jpg files to a review folder after conversion is complete.
-mkdir _review && mv ./*.jpg _review
+mkdir _review && mv ./*.jp*g _review
--- Scripts/JXL/jpg-jxl.sh
+++ Scripts/JXL/jpg-jxl.sh
@@ -3,5 +3,7 @@
 # Use this to optimize your existing .jpg losslessly!
 # NOTE: You need the "cjxl" tool to run this!
 
+# Find all JPG files in current directory, use the CJXL tool to losslessly convert to JXL
 for f in ./*.jp*g ; do cjxl --quiet --lossless_jpeg=1 "$f" "${f%.jp*g}.jxl" ; done
+# Create directory _review and then move all JPG files into it
 mkdir _review && mv *.jp*g _review
